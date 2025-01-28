698f587c9d9dd14595f720e39be831e51f726b87
kghent4 <130794676+kghent4@users.noreply.github.com>
2025-01-28 16:17:53 -0600

    Create Player.java

---
+++ src/Player.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+public class Player {
+
+    //TODO FIX
+    public String getName(){
+        return "no one";
+    }
+
+    ///TODO FIX
+    public int getMoney(){
+        return 1000;
+    }
+
+    //TODO FIX
+    public ArrayList<Property> getProperties(){
+        return null;
+    }
+
+    //TODO FIX
+    //Go is the top left corner, location 0. Locations increase by 1 for each property that the player passes clockwise.
+    public int getLocation(){
+        return 0;
+    }
+}
